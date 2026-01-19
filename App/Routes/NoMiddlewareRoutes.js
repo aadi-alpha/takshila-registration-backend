8370920b11f2bfc4dfcf336d4db404e2e97f0f88
aadi-alpha <aadinagpal46@gmail.com>
2026-01-19 20:30:59 +0530

    .

--- App/Routes/NoMiddlewareRoutes.js
+++ App/Routes/NoMiddlewareRoutes.js
@@ -3,11 +3,13 @@ const express = require('express')
 const { UserTakshilaLogin } = require('../Controllers/UserTakshila')
 const {CreateBatch , BatchFetch}=require('../Controllers/BatchController')
 const { AllBranchesGet } = require('../Controllers/BranchController')
+const { insertStudents } = require('../Controllers/StudentRegisterController')
 
 
 
 const NoMiddleWareRoutes = express.Router()
 NoMiddleWareRoutes.get('/branch-fetch', AllBranchesGet)
+NoMiddleWareRoutes.post('/student-insert', insertStudents)
 NoMiddleWareRoutes.post('/UserTakshila-fetch-login', UserTakshilaLogin)
 NoMiddleWareRoutes.post('/batch-insert', CreateBatch)
 NoMiddleWareRoutes.get('/batch-fetch', BatchFetch)
--- App/Routes/RegistrationRoutes.js
+++ App/Routes/RegistrationRoutes.js
@@ -7,6 +7,5 @@ const upload = require('../../multer')
 
 let StudentRegisterRouter = express.Router()
 StudentRegisterRouter.post('/image-upload',upload.single("photo"),insertImage)
-StudentRegisterRouter.post('/student-insert', insertStudents)
 
 module.exports = { StudentRegisterRouter }
